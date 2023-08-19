7bb464974471addce1cf73e874b2fa284dfb2bdb
MartinFarro2020 <martin.farro.martinez@gmail.com>
2023-08-19 17:18:02 -0500

    animation logo

--- src/components/LocationForm.jsx
+++ src/components/LocationForm.jsx
@@ -4,8 +4,9 @@ const LocationForm = ({handleSubmit}) => {
     <section className="grid grid-cols-1 justify-center bg-[url('/image/bg-header.png')] items-center mx-w-[1200px]">
 
       <section className="flex flex-col items-center">
-        <img className="relative min-w-fit h-64" src="/images/portal.png"/>
-        <img className="absolute w-72 h-28 pt-6" src="/images/logo.png"/>
+        <img className="relative min-w-fit h-64" src="/images/portal04.gif"/>
+        {/* animate-spin transiton duration-1000 */}
+        <img className="absolute w-72 h-28 mt-14" src="/images/logo.png"/>
       </section>
       <div className="flex flex-col justify-center items-center gap-4">
           <form onSubmit={handleSubmit} className="flex-row w-76 h-12 border-[#8EFF8B] border-2">
